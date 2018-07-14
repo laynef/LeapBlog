9c6f4d03842022b432665f21c56502621b06a84e
Layne Faler <laynefaler@gmail.com>
2018-07-14 01:12:10 -0700

    Updates to header for login link

--- web/app/publicPages/components/globals/components/Header.js
+++ web/app/publicPages/components/globals/components/Header.js
@@ -18,7 +18,7 @@ export default class Header extends Component {
                     <a href="https://www.linkedin.com/in/laynefaler/"><i className="fa fa-linkedin" /></a>
                     <a href="https://github.com/laynef/LeapBlogger"><i className="fa fa-github" /></a>
                     <a href="http://www.manoftech.com/"><i className="fa fa-cog" /></a>
-                    <a href="/login">Sign Up | Login</a>
+                    <a href={`/${process.env.BASE_MAIN_URL}/login`}>Sign Up | Login</a>
                 </div>
 			</header>
 		);
