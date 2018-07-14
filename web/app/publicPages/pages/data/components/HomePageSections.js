cd90e868d90fd1461b169435cbcc6dce1345f228
Layne Faler <laynefaler@gmail.com>
2018-07-14 13:29:41 -0700

    Updates to content for header sections

--- web/app/publicPages/pages/data/components/HomePageSections.js
+++ web/app/publicPages/pages/data/components/HomePageSections.js
@@ -1,14 +1,14 @@
 module.exports = [
     {
-        header_title: 'Get started on your first blog',
+        header_title: 'Get started',
         header_description: 'Easy ways to make blogs and experience yourself to people around the world',
         first_card_title: 'Title',
         first_card_description: 'Description',
         second_card_display: '',
     },
     {
-        header_title: 'Get started on your first blog',
-        header_description: 'Easy ways to make blogs and experience yourself to people around the world',
+        header_title: 'Share your posts',
+        header_description: 'Show off your posts to the people important to you or all around the world',
         first_card_title: 'Title',
         first_card_description: 'Description',
         second_card_display: '',
