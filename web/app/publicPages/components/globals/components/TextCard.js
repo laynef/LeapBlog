662584c1a8f91fe1bd65d3357f8b620599690969
Layne Faler <laynefaler@gmail.com>
2018-07-14 13:30:48 -0700

    Updates to text card for font sizes

--- web/app/publicPages/components/globals/components/TextCard.js
+++ web/app/publicPages/components/globals/components/TextCard.js
@@ -14,7 +14,7 @@ export default class TextCard extends Component {
         const { title, description } = this.props;
 		return (
 			<div className="card w-100 m-2 p-3">
-				<h1 className="h3">{title}</h1>
+				<h1 className="h5">{title}</h1>
 				<h1 className="h6">{description}</h1>
 			</div>
 		);
