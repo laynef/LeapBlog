76706cdde5c9363ad4586663612d43f46fcb2c73
Layne Faler <laynefaler@gmail.com>
2018-07-14 19:13:24 -0700

    Updates to footer for more links

--- web/app/publicPages/components/globals/components/Footer.js
+++ web/app/publicPages/components/globals/components/Footer.js
@@ -13,11 +13,15 @@ export default class Footer extends Component {
 	render() {
 		return (
 			<footer>
-				<h1 className="text-center h4">Leap Blog</h1>
-                <div className="w-25 f-sb">
-                    <a target="_blank" href="https://www.linkedin.com/in/laynefaler/"><i className="fa fa-linkedin" /></a>
-                    <a target="_blank" href="https://github.com/laynef/LeapBlogger"><i className="fa fa-github" /></a>
-                    <a target="_blank" href="http://www.manoftech.com/"><i className="fa fa-cog" /></a>
+				<div className="w-100 f-sa m-2">
+                    <a className="h5 text-center w-33" href={`/${process.env.BASE_API_URL}/docs`}>API Docs</a>
+                    <a className="h5 text-center w-33" href={`mailto:laynefaler@gmail.com`}>Contact</a>
+                    <a className="h5 text-center w-33" href={`/`}>Other Frameworks</a>
+                </div>
+                <div className="w-50 f-sa">
+                    <a className="hort-center" target="_blank" href="https://www.linkedin.com/in/laynefaler/"><i className="fa fa-linkedin" /></a>
+                    <a className="hort-center" target="_blank" href="https://github.com/laynef/LeapBlogger"><i className="fa fa-github" /></a>
+                    <a className="hort-center" target="_blank" href="http://www.manoftech.com/"><i className="fa fa-cog" /></a>
                 </div>
 			</footer>
 		);
