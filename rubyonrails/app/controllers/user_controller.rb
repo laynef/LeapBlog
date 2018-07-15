5480781dbc3359447d8e280754f4511e8b279f74
Layne Faler <laynefaler@gmail.com>
2018-07-15 11:26:45 -0700

    Updates to user controller

--- rubyonrails/app/controllers/user_controller.rb
+++ rubyonrails/app/controllers/user_controller.rb
@@ -1,2 +1,40 @@
+require 'bcrypt'
+require 'jwt'
+
 class UserController < ApplicationController
+
+    include BCrypt
+    include JWT
+
+    def login
+
+    end
+
+    def register
+
+    end
+
+    def register_verify
+
+    end
+
+    def forgotten_password
+
+    end
+
+    def forgotten_password_verify
+
+    end
+
+    private
+    def hasher
+        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQSTRUVWXYZ1234567890-=!@#$%^&*()_+"
+        hash = ''
+        for (0..40) do
+            idx = rand(chars.length) + 1
+            hash += chars[idx]
+        end
+        return hash
+    end
+
 end
