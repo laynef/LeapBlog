a5bf79523d90aa03351ddb82c51dd5387a5da6bf
Layne Faler <laynefaler@gmail.com>
2018-07-15 17:28:51 -0700

    Updates to database schema

--- rubyonrails/db/schema.rb
+++ rubyonrails/db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_15_235936) do
+ActiveRecord::Schema.define(version: 2018_07_16_002713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ads", force: :cascade do |t|
+    t.bigint "category_id"
+    t.string "image_url"
+    t.string "target_url"
+    t.text "description"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_ads_on_category_id"
+  end
 
   create_table "blogs", force: :cascade do |t|
     t.text "text"
@@ -80,6 +91,7 @@ ActiveRecord::Schema.define(version: 2018_07_15_235936) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "ads", "categories"
   add_foreign_key "blogs", "categories"
   add_foreign_key "blogs", "users"
   add_foreign_key "comments", "users"
